1f4a1eb0229124a4ea397a9728520bed984b3894
Alimossim <agaev.alimosim@gmail.com>
2022-10-31 14:35:01 +0600

    feat: solve task #2 write file

---
+++ 02-write-file/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+const readline = require('readline');
+const rl = readline.createInterface
+  (
+    process.stdin,
+    process.stdout
+  );
+const dir = path.dirname(__filename);
+const output = fs.createWriteStream(path.join(dir, 'text.txt'));
+
+
+rl.on('line', (input) => {
+  output.write(input);
+  if (input == 'exit') {
+    console.log('Программа завершена');
+    process.exit();
+  }
+});
+
+rl.on('SIGINT', (code) => {
+  console.log('Программа завершена');
+  process.exit();
+});
