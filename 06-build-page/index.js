6e4946eff70b44e3e481e85985b1452e6e360e15
Alimossim <agaev.alimosim@gmail.com>
2022-11-03 14:40:29 +0600

    feat add css bundler and copy assats  function

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -6,7 +6,8 @@ const { count } = require('console');
 const templateDir = path.join(__dirname, 'template.html');
 const componentsDir = path.join(__dirname, `components`);
 const distDir = path.join(__dirname, 'project-dist');
-
+const bundle = fs.createWriteStream(path.join(distDir, 'style.css'));
+const stylesDir = path.join(__dirname, 'styles');
 
 fs.mkdir(distDir, {
   recursive: true
@@ -54,3 +55,70 @@ templateReader.on('end', () => {
 
 
 
+
+
+let cssData = '';
+fs.readdir(stylesDir, (err, files) => {
+  if (err) {
+    console.log(err);
+  } else {
+    files.forEach(file => {
+      let fileDir = path.join(stylesDir, file);
+      let fileNameWithoutExt = path.basename(fileDir, path.extname(fileDir));
+      let fileExt = path.extname(fileDir).slice(1);
+      fs.stat(fileDir, (err, stats) => {
+        if (!stats.isDirectory() && fileExt === 'css') {
+          const readableStream = fs.createReadStream(fileDir, 'utf-8');
+          readableStream.on('data', chunk => cssData += chunk);
+          readableStream.on('end', () =>  bundle.write(cssData));
+          readableStream.on('error', error => console.log('Error', error.message));
+        }
+      });
+    });
+  }
+});
+
+
+
+const fsPromises = fs.promises;
+function createFolder(dir,foldername){
+  fs.mkdir(path.join(dir,foldername),{ recursive: true },(err)=>{
+    if(err){
+      console.log(err);
+    }
+  });
+}
+function copyFile(src,dest){
+  fsPromises.copyFile(src,dest)
+    .catch(function(error){
+      console.log(error);
+    });
+}
+
+function CopyDir(src,dest){
+  fs.readdir(src, (err, files) => {
+    if (err) {
+      console.log(err);
+    } else {
+      files.forEach(file => {
+        let fileDir = path.join(src, file);
+        fs.stat(fileDir, (err, stats) => {
+          if (!stats.isDirectory()) {
+            copyFile(fileDir, path.join(dest,file));
+          } else{
+            createFolder(dest,path.basename(fileDir));
+            console.log(fileDir,path.join(dest,path.basename(fileDir)));
+            CopyDir(fileDir,path.join(dest,path.basename(fileDir)));
+          }
+        });
+      });
+     
+    }
+    
+  });
+  return 'Копирование завершено';
+}
+
+const assetsDir = path.join(__dirname, 'assets');
+const assetsDist = path.join(__dirname,'project-dist', 'assets');
+CopyDir(assetsDir,assetsDist);
