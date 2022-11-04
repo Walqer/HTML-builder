0f0e500a83092adb8cbeda0bc43d2946372995d9
Alimossim <agaev.alimosim@gmail.com>
2022-11-04 21:18:29 +0600

    refactor: remove var name

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const dir = (path.dirname(__filename));
-const readableStream = fs.createReadStream(path.join(dir,'text.txt'), 'utf-8');
+const readableStream = fs.createReadStream(path.join(__dirname,'text.txt'), 'utf-8');
 let data = '';
 readableStream.on('data', chunk => data+=chunk);
 readableStream.on('end', () => console.log(data));
