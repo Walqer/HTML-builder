695f32f95a03dfcf157d699b248500f21feea768
Alimossim <agaev.alimosim@gmail.com>
2022-11-05 13:29:02 +0600

    fix writing in bundle.css

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -7,19 +7,27 @@ const dist = path.join(path.dirname(__filename), 'project-dist');
 const bundle = fs.createWriteStream(path.join(dist, 'bundle.css'));
 
 let data = '';
-fs.readdir(dir, (err, files) => {
+let merge = fs.readdir(dir, (err, files) => {
   if (err) {
     console.log(err);
   } else {
+    let counter = 0;
+    let mergedCounter = 0;
     files.forEach(file => {
       let fileDir = path.join(dir, file);
       let fileNameWithoutExt = path.basename(fileDir, path.extname(fileDir));
       let fileExt = path.extname(fileDir).slice(1);
       fs.stat(fileDir, (err, stats) => {
         if (!stats.isDirectory() && fileExt === 'css') {
+          counter++;
           const readableStream = fs.createReadStream(fileDir, 'utf-8');
           readableStream.on('data', chunk => data+= chunk);
-          readableStream.on('end', () =>  bundle.write(data));
+          readableStream.on('end', () =>{
+            mergedCounter++;
+            if(mergedCounter === counter){
+              bundle.write(data);
+            }
+          });
           readableStream.on('error', error => console.log('Error', error.message));
         }
       });
